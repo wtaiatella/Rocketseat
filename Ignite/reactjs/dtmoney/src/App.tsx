8bdad04ce153faca65bbc73ee83fecff9deedd8a
Wagner Taiatella <wtaiatella@gmail.com>
2022-04-23 15:22:17 -0400

    feat: dtmoney: add styled-components library

--- Ignite/reactjs/dtmoney/src/App.tsx
+++ Ignite/reactjs/dtmoney/src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+	color: #8257e6;
+	font-size: 64px;
+`;
+
 export function App() {
 	return (
 		<div className='App'>
-			<h1>Hello world!!</h1>
+			<Title>Hello world!!</Title>
 		</div>
 	);
 }
