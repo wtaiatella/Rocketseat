ed319246d94e20a8f9258663b8dea0c9ca91b45e
Wagner Taiatella <wtaiatella@gmail.com>
2022-04-26 07:49:17 -0400

    chore:dtmoney: add Transaction table component

--- Ignite/reactjs/dtmoney/src/components/TransactionTable/index.tsx
+++ Ignite/reactjs/dtmoney/src/components/TransactionTable/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { api } from '../../services/api';
-import { Container } from './styled';
+import { Container } from './styles';
 
 export function TransactionTable() {
 	useEffect(() => {
