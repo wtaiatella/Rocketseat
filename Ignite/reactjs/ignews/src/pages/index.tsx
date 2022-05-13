631045bb93c546d618c57c17d76ce27c576f3223
Wagner Taiatella <wtaiatella@gmail.com>
2022-05-13 15:40:23 -0400

    chore:ignews: add stripe lib and component

--- Ignite/reactjs/ignews/src/pages/index.tsx
+++ Ignite/reactjs/ignews/src/pages/index.tsx
@@ -1,9 +1,17 @@
+import { GetStaticProps } from 'next';
 import styles from './home.module.scss';
 import Head from 'next/head';
 import Image from 'next/image';
 import SubscribeButton from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface homeProps {
+	product: {
+		priceId: string;
+		amount: number;
+	};
+}
+export default function Home({ product }: homeProps) {
 	return (
 		<>
 			<Head>
@@ -17,9 +25,9 @@ export default function Home() {
 					</h1>
 					<p>
 						Get access to all the publications <br />
-						<span>for $9.90 month</span>
+						<span>for {product.amount} month</span>
 					</p>
-					<SubscribeButton />
+					<SubscribeButton priceId={product.priceId} />
 				</section>
 
 				<Image
@@ -33,3 +41,26 @@ export default function Home() {
 		</>
 	);
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+	const price = await stripe.prices.retrieve(
+		'price_1Kz03YJqQAyuGGLnMapndLie',
+		{
+			expand: ['product'],
+		}
+	);
+
+	const product = {
+		priceId: price.id,
+		amount: new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'USD',
+		}).format(price.unit_amount / 100),
+	};
+	return {
+		props: {
+			product,
+		},
+		revalidate: 60 * 60 * 24, //24horas
+	};
+};
