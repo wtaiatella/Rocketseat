79d69efe9467ab2f9c39d388dfe14b9592eba2ce
Wagner Taiatella <wtaiatella@gmail.com>
2022-05-17 16:53:28 -0400

    seat:ignews: add github login with nextauth

--- Ignite/reactjs/ignews/src/components/SignInButton/index.tsx
+++ Ignite/reactjs/ignews/src/components/SignInButton/index.tsx
@@ -3,17 +3,31 @@ import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 export function SingInButton() {
 	const isUserLoggedIn = true;
 
-	return isUserLoggedIn ? (
-		<button type='button' className={styles.signInButton}>
+	const { data: session } = useSession();
+
+	console.log(session);
+
+	return session ? (
+		<button
+			type='button'
+			className={styles.signInButton}
+			onClick={() => signOut()}
+		>
 			<FaGithub color='#04d361' />
-			Diego Fernandes
+			{session.user.name}
 			<FiX color='#737380' className={styles.closeIcon} />
 		</button>
 	) : (
-		<button type='button' className={styles.signInButton}>
+		<button
+			type='button'
+			className={styles.signInButton}
+			onClick={() => signIn('github')}
+		>
 			<FaGithub color='#eba417' />
 			Sign in with Github
 		</button>
