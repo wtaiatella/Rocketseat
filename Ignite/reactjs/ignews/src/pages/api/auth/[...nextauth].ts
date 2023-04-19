01a2e81c17aa48af28f696dabda3a71200f71d76
Wagner Taiatella <wtaiatella@gmail.com>
2023-04-19 15:16:10 -0300

    chore: nextauth config

--- Ignite/reactjs/ignews/src/pages/api/auth/[...nextauth].ts
+++ Ignite/reactjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,52 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
+import { FaUnderline } from 'react-icons/fa';
+
 export default NextAuth({
 	// Configure one or more authentication providers
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			authorization: { params: { scope: 'user' } },
+			//authorization: { params: { scope: 'user' } },
 		}),
 		// ...add more providers here
 	],
+
+	/* 	jwt: {
+		signingKey: process.env.NEXTAUTH_SECRET,
+	}, */
+
+	callbacks: {
+		async signIn({ user, account, profile, email, credentials }) {
+			const e_mail = user.email;
+
+			/* 			try {
+								await fauna.query(
+					q.If(
+						q.Not(
+							q.Exists(
+								q.Match(
+									q.Index('user_by_email'),
+									q.Casefold(user.email)
+								)
+							)
+						),
+						q.Create(
+							q.Collection('users'), 
+							{ data: { e_mail } }
+						)
+						q.Update
+					) 
+				);
+				return true;
+			} catch {
+				return false;
+			} */
+			return true;
+		},
+	},
 });
