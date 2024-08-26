0ba1a09821aad52d440cec569f022ab174494af1
Wagner Taiatella <wtaiatella@gmail.com>
2024-08-26 14:30:34 -0300

    feat: ignite-react 03-dtMoney: ajustes iniciais e página transactions.

--- ignite-react/03-dtMoney/src/App.tsx
+++ ignite-react/03-dtMoney/src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Transactions />
+    </ThemeProvider>
   );
 }
