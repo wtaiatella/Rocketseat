a9c7bf05108d0afab7a6d91846eff6c4a6c29ea4
Wagner Taiatella <wtaiatella@gmail.com>
2024-08-29 10:33:25 -0300

    pages: ignite-react 04pizzashop: adição pagina login

--- ignite-react/04-pizza-shop/src/pages/auth/SignIn.tsx
+++ ignite-react/04-pizza-shop/src/pages/auth/SignIn.tsx
@@ -1,7 +1,34 @@
+import { Helmet } from "react-helmet-async";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export function SignIn() {
   return (
-    <div>
-      <h1>signI n</h1>
-    </div>
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className="w-88 flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro'
+            </p>
+          </div>
+          <form className="flex flex-col gap-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input type="email" id="email" />
+            </div>
+            <Button type="submit" className="">
+              Acessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
