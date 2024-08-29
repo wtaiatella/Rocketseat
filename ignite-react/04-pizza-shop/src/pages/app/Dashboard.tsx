b2414eb900402294566a8c9ce70fd5c8dbf51424
Wagner Taiatella <wtaiatella@gmail.com>
2024-08-29 10:25:32 -0300

    feat: ignite-react-04pizzaShop: instalação e configuração lib helmet

--- ignite-react/04-pizza-shop/src/pages/app/Dashboard.tsx
+++ ignite-react/04-pizza-shop/src/pages/app/Dashboard.tsx
@@ -1,7 +1,10 @@
+import { Helmet } from "react-helmet-async";
+
 export function Dashboard() {
   return (
-    <div>
+    <>
+      <Helmet title="Dashboard" />
       <h1>dashboard</h1>
-    </div>
+    </>
   );
 }
